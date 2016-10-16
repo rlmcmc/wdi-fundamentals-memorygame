8590e766e2ef0ee2bba264d5801b64577a6bb733
Robert Mathews III <robertmathewsiii@Roberts-MacBook-Pro-2.local>
2016-10-16 18:31:33 -0400

    finished chapter 10

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,25 @@
-console.log("JS file is connected to HTML! Woo!")
-
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+//if (cardOne == cardTwo) {
+  //  alert("You found a match");
+//}
+//else if (cardThree == cardFour) {
+ // alert("You found a match"); 
+//}
+//else {
+ // alert("Sorry, try again");
+//} 
+
+
+var createCards = function() {
+	var gameBoard = document.getElementById('game-board');
+	for (var i = 0; i < 4; i++) {
+		var newDiv = document.createElement('div');
+		newDiv.className = "card";		
+		gameBoard.appendChild(newDiv);
+	}
+}
+
